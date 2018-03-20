b04a218dd7166422eadf1b8ea28628a5b9fd81d5
Dcodefrenzy <ayodejisamuelfakunle@gmail.com>
2018-03-20 14:43:08 +0100

    creating Reg

---
+++ reg.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>register</title>
+</head>
+<body>
+  <?php
+  echo "<p>Hello world</p>"
+  echo "<p>Hello world</p>"
+  echo "<p>Hello world</p>"
+  echo "<p>Hello world</p>"
+  ?>
+
+</body>
+</html>
