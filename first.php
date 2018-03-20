98446a15f2b085c379f5d6659b12b22c140ce5b9
Dcodefrenzy <ayodejisamuelfakunle@gmail.com>
2018-03-20 11:29:37 +0100

    My first commit

---
+++ first.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>git</title>
+  </head>
+  <body>
+  <h1>hello git</h1>
+  <p>my first time using git</p>
+  </body>
+</html>
